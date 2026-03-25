e427c7c22ac9cf221925ac1a1ac6223bd8592ee3
ph-value <phvalue123@gmail.com>
2026-03-25 14:11:40 +0900

    feat: [에이전트] 에이전트 생성 페이지 네비게이션 테스트 추가

---
+++ tests/agent/test_agent_create.py
@@ -0,0 +1,35 @@
+import pytest
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from locators.menu_locators import *
+
+
+def test_navigate_to_agent_create(navigate_to_agent_explore, wait):
+    # Arrange
+    driver = navigate_to_agent_explore
+
+    # Act
+    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href='/ai-helpy-chat/agents/builder']"))).click()
+    create_title = wait.until(EC.presence_of_element_located((By.XPATH, "//p[text()='새 에이전트 만들기']")))
+
+    # Assert
+    assert create_title.text == "새 에이전트 만들기"
+
+
+
+
+
+# def test_register_form_is_visible():
+#     pass
+# def test_register_required_toggles_are_present():
+#     pass
+# def test_register_toggles_are_interactable():
+#     pass
+# def test_register_image_upload_button_is_enabled():
+#     pass
+# def test_register_singleline_input_is_editable():
+#     pass
+# def test_register_multiline_input_is_editable():
+#     pass
+# def test_register_required_fields_are_rendered():
+#     pass
