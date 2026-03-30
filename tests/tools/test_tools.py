8a08ccdf3a2b728105f871b8abba98db35389104
ph-value <phvalue123@gmail.com>
2026-03-30 16:15:08 +0900

    refactor: [도구] 메뉴 이동을 위해 하드코딩된 선택자를 로케이터로 대체

--- tests/tools/test_tools.py
+++ tests/tools/test_tools.py
@@ -2,6 +2,8 @@ import pytest
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+from locators.menu_locators import MENU_H2_TITLE, MENU_TOOLS
+
 
 def test_navigate_to_tool_menu(logged_in_driver, wait):
     #==========
@@ -14,10 +16,10 @@ def test_navigate_to_tool_menu(logged_in_driver, wait):
     # Act
     #==========
     # 도구 페이지 진입 
-    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href='/ai-helpy-chat/tools']"))).click()
+    wait.until(EC.element_to_be_clickable(MENU_TOOLS)).click()
 
     #==========
     # Assert
     #==========
-    tools_title = wait.until(EC.presence_of_element_located((By.TAG_NAME,"h2")))
+    tools_title = wait.until(EC.presence_of_element_located(MENU_H2_TITLE))
     assert tools_title.text == "도구 목록"
