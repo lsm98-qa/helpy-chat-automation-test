d264f01cb6c1ff4d8ad2c44d50869424ac2f62e7
hyeonwoo ko <sich136@gmail.com>
2026-03-30 13:18:37 +0900

    feat : [채팅] 이름 변경 테스트 - 채팅 미존재 예외 처리 추가

--- tests/chat/test_rename_chat.py
+++ tests/chat/test_rename_chat.py
@@ -1,13 +1,14 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from pages.chat_actions import click_top_chat_item_option_button, get_top_chat_item
+from pages.chat_actions import click_top_chat_item_option_button, get_top_chat_item, check_chat_exists_or_pass
 
 def rename_chat(logged_in_driver, wait):
     #==========
     # Arrange
     #==========
     logged_in_driver
+    check_chat_exists_or_pass(wait)
     
     #==========
     # Act
