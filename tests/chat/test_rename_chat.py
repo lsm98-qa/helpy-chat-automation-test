d6f194302061c2c903705b151130be7c7e23b929
hyeonwoo ko <sich136@gmail.com>
2026-04-01 15:57:18 +0900

    fix : [채팅] 삭제, 이름 변경 테스트 - 채팅 목록 미존재 상태 검증 삭제, 미존재인 경우 새대화 1개 추가 구현, 이름 변경 / 삭제 메뉴 클릭 코드 보완

--- tests/chat/test_rename_chat.py
+++ tests/chat/test_rename_chat.py
@@ -1,10 +1,11 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from pages.chat_actions import click_top_chat_item_option_button, _get_top_chat_item, get_top_chat_item_or_none
-import pytest
-from selenium.webdriver.common.action_chains import ActionChains
+from pages.chat_actions import click_top_chat_item_option_button, get_top_chat_item_or_none, click_new_chat, send_chat_message, _get_top_chat_item
 
+# =========================
+# 이름 변경 기능 사용 시 정상적으로 변경이 되는 지 검증
+# =========================
 def test_can_rename_top_chat_item(logged_in_driver, wait):
     #==========
     # Arrange
@@ -14,20 +15,25 @@ def test_can_rename_top_chat_item(logged_in_driver, wait):
 
     # 채팅 목록 확인
     chat = get_top_chat_item_or_none(wait)
-    if chat is None:
-        pytest.skip("채팅 기록이 존재하지 않습니다.")
+    
 
     
     #==========
     # Act
     #==========
+
+    if chat is None:
+        click_new_chat(wait)
+        send_chat_message(wait, "A")
+
     # 채팅 옵션 열기
     click_top_chat_item_option_button(wait)
 
     # 이름 변경 메뉴 클릭
-    wait.until(
-    lambda d: d.find_element(By.XPATH, "//li[normalize-space()='이름 변경']")
-    ).click()
+    rename_menu = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//li[normalize-space()='이름 변경']"))
+    )
+    rename_menu.click()
 
     # 이름 입력창 확인
     input_box = wait.until(lambda d: d.find_element(By.CSS_SELECTOR,
@@ -54,48 +60,9 @@ def test_can_rename_top_chat_item(logged_in_driver, wait):
     #==========
     # 최상단 채팅 요소 가져오기
     chat_item = _get_top_chat_item(wait)
-    
+
     # 변경된 채팅 이름 확인
     wait.until(EC.invisibility_of_element(input_box)) # 이름 입력창 사라질 때 까지 대기
     assert new_name == chat_item.text.strip(), "입력한 이름과 변경된 이름이 일치하지 않습니다."
-
-def test_chat_options_hidden_when_chat_list_empty(logged_in_driver, wait):
-    #==========
-    # Arrange
-    #==========
-    # 로그인
-    logged_in_driver
-    # 채팅 목록이 비어있는지 확인
-    chat = get_top_chat_item_or_none(wait)
-    if chat is not None:
-        pytest.skip("채팅 기록이 1개 이상 존재합니다.")
-
-    driver = wait._driver
-
-    # 채팅 영역 확인
-    sidebar = wait.until(
-        lambda d: d.find_element(By.CSS_SELECTOR, "aside")
-    )
-
-    chat_list_area = sidebar.find_element(
-        By.CSS_SELECTOR, "[data-testid='virtuoso-scroller']"
-    )
-
-    #==========
-    # Act
-    #==========
-    # 채팅 영역에 마우스 이동
-    ActionChains(driver).move_to_element(chat_list_area).perform()
-
-    #==========
-    # Assert
-    #==========
-    # 옵션 버튼이 보이지 않는지 확인
-    option_button = driver.find_elements(
-        By.CSS_SELECTOR,
-        "[data-testid='virtuoso-scroller'] svg[data-icon='ellipsis-vertical']",
-    )
-
-    assert len(option_button) == 0, "채팅 기록이 없는 상태에서 옵션 버튼이 확인됩니다."
 
     
